e5a6eeb397fede6c2f5c760f98f6de8238470c34
Armando <flores@mtu.edu>
2015-12-31 00:38:26 -0800

    Waypoints makes things sexier, but still too much animations yo!

---
+++ 1/js/waypoints.js
@@ -0,0 +1,13 @@
+
+  $('#product-trigger').waypoint( function(){
+    document.getElementById("products").classList.add("active");
+  });
+
+  $('#trending-trigger').waypoint( function(){
+    document.getElementById("trending").classList.add("active");
+  });
+
+  $('#blog-trigger').waypoint( function(){
+    console.log("Heyy beautifullz")
+    document.getElementById("blog").classList.add("active");
+  });
